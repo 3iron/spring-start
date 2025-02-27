6b1a71e0617a89c654fb3cc95144daf2d76b5854
pmj0314 <pmj@artl.co.kr>
2025-02-27 18:15:20 +0900

    git test

---
+++ src/main/groovy/hello/hello_spring/HelloSpringApplication.groovy
@@ -0,0 +1,14 @@
+package hello.hello_spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HelloSpringApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication, args)
+		println("git test");
+	}
+
+}
